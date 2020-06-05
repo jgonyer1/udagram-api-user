76b6bb040f657001d1be010c037ad83366fe9cb7
Jacob Gonyer <jgonyer@darlingconsulting.com>
2020-06-05 12:42:37 -0400

    Removed references to feed api

--- src/controllers/v0/users/routes/user.router.ts
+++ src/controllers/v0/users/routes/user.router.ts
@@ -10,7 +10,9 @@ router.use('/auth', AuthRouter);
 router.get('/');
 
 router.get('/:id', async (req: Request, res: Response) => {
+  console.log("user get");
   const {id} = req.params;
+  console.log("ID: " + id);
   const item = await User.findByPk(id);
   console.log(item);
   res.send(item);
